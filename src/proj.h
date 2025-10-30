7941563ff883819c2ff8fbdd8611badb05a6dda4
GerhardMitterbaur <63344758+mgerhard74@users.noreply.github.com>
2025-10-30 19:33:11 +0100

    Update proj.h

--- src/proj.h
+++ src/proj.h
@@ -30,7 +30,7 @@
 #define AP_PIN 14
 //#define OTA
 //#define STROMPREIS
-#define VERSION "1.4.4"
+#define VERSION "1.4.5"
 #define APP_NAME "Amis"
   extern String dbg_string;
   extern char dbg[128];
