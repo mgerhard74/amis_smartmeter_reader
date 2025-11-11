97d37b8580ee32d3db664c7de535031328a3dd42
Stefan Oberhumer <stefan@obssys.com>
2025-11-11 11:31:46 +0100

    NFC: Use debug.h everywhere

--- src/wsock.cpp
+++ src/wsock.cpp
@@ -2,23 +2,7 @@
 #include "AmisReader.h"
 
 //#define DEBUG
-#ifndef DEBUG
-  #define eprintf( fmt, args... )
-  #define DBGOUT(...)
-#else
-  #if DEBUGHW>0
-    #define FOO(...) __VA_ARGS__
-    #define DBGOUT dbg_string+= FOO
-    #if (DEBUGHW==2)
-      #define eprintf(fmt, args...) S.printf(fmt, ##args)
-    #elif (DEBUGHW==1 || DEBUGHW==3)
-      #define eprintf(fmt, args...) {sprintf(dbg,fmt, ##args);dbg_string+=dbg;dbg[0]=0;}
-    #endif
-  #else
-    #define eprintf( fmt, args... )
-    #define DBGOUT(...)
-  #endif
-#endif
+#include "debug.h"
 
 extern const char *__COMPILED_DATE_TIME_UTC_STR__;
 
