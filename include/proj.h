a2ae126920524d81b50b796c6efa17a90d21445f
Stefan Oberhumer <stefan@obssys.com>
2025-12-02 18:47:48 +0100

    Bump version to 1.4.7

--- include/proj.h
+++ include/proj.h
@@ -28,7 +28,7 @@
 #define AP_PIN 14
 //#define OTA
 //#define STROMPREIS
-#define VERSION "1.4.6"
+#define VERSION "1.4.7"
 #define APP_NAME "Amis"
   extern String dbg_string;
   extern char dbg[128];
