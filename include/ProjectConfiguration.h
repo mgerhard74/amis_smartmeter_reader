c5b70e044bd7ec2a448fd43b8f21f28b25aa7eae
Stefan Oberhumer <stefan@obssys.com>
2026-01-02 17:30:45 +0100

    Bump version to 1.5.5

--- include/ProjectConfiguration.h
+++ include/ProjectConfiguration.h
@@ -2,7 +2,7 @@
 
 // Eckdaten der Applikation
 #define APP_NAME        "Amis"
-#define APP_VERSION_STR "1.5.4"
+#define APP_VERSION_STR "1.5.5"
 
 
 // Pin, mit dem der Zähler mittels Jumper auf Masse
