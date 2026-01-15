6931f6100d63953694285c4876c0c11b0466e13f
Stefan Oberhumer <stefan@obssys.com>
2026-01-15 14:47:50 +0100

    Startup: Adapt logging.

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,17 @@ void setup() {
 
     Config.applySettingsConfigGeneral();
 
-  // Start Network
+    // Developer: Enable verbose logging on some modules
+#if 0
+    Log.setLoglevel(LOGLEVEL_VERBOSE, LOGMODULE_AMISREADER);
+    Log.setLoglevel(LOGLEVEL_VERBOSE, LOGMODULE_MODBUS);
+    Log.setLoglevel(LOGLEVEL_VERBOSE, LOGMODULE_MQTT);
+    Log.setLoglevel(LOGLEVEL_VERBOSE, LOGMODULE_THINGSPEAK);
+    Log.setLoglevel(LOGLEVEL_VERBOSE, LOGMODULE_WATCHDOGPING);
+    Log.setLoglevel(LOGLEVEL_VERBOSE, LOGMODULE_REMOTEONOFF);
+#endif
+
+    // Start Network
     Network.init(Application.inAPMode());
     NetworkConfigWifi_t networkConfigWifi = Network.getConfigWifi();
     Network.connect();
@@ -158,7 +168,7 @@ void setup() {
     ModbusSmartmeterEmulation.init();
     if (Config.smart_mtr) {
         ModbusSmartmeterEmulation.enable();
-        LOG_VP("ModbusSmartmeterEmulation enabled");
+        LOG_DP("ModbusSmartmeterEmulation enabled");
     }
 
     // Shelly Smart Meter Emulator
@@ -174,7 +184,7 @@ void setup() {
     WatchdogPing.config(networkConfigWifi.pingrestart_ip, networkConfigWifi.pingrestart_interval, networkConfigWifi.pingrestart_max);
     if (networkConfigWifi.pingrestart_do) {
         WatchdogPing.enable();
-        LOG_VP("WatchdogPing enabled");
+        LOG_DP("WatchdogPing enabled");
     }
 
     // Netzwerksteckdose (On/Off via Netzwerk)
@@ -193,14 +203,14 @@ void setup() {
     RebootAtMidnight.config();
     if (Config.reboot0) {
         RebootAtMidnight.enable();
-        LOG_VP("RebootAtMidnight enabled");
+        LOG_DP("RebootAtMidnight enabled");
     }
 
     secTicker.attach_scheduled(1, secTick);
 
-    LOG_IP("System setup completed, running");
-
     SYSTEMMONITOR_STAT();
+
+    LOG_IP("System setup completed, running");
 }
 
 void loop() {
@@ -239,6 +249,7 @@ void loop() {
     Log.loop(); // Eine Seite des Logfiles an einen Websocket client senden
 
     LedBlue.loop();
+
     WatchdogPing.loop();
 
     MDNS.update();
@@ -251,6 +262,7 @@ void loop() {
         Serial.flush();
         doSerialHwTest = false;
     }
+
     SYSTEMMONITOR_STAT();
 }
 
