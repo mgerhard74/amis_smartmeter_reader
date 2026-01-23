d421f9d19def1a2bf3ec6463dcca89e97386bd69
Karl Kupferfuchs <koarli.social@mailbox.org>
2026-01-23 12:56:11 +0100

    fixed unsecure char*, removed unused vars, fixed programming style(namings, early exit)

--- src/ShellySmartmeterEmulation.cpp
+++ src/ShellySmartmeterEmulation.cpp
@@ -16,6 +16,7 @@ ShellySmartmeterEmulationClass::ShellySmartmeterEmulationClass()
 
 void ShellySmartmeterEmulationClass::init(Device device, int offset)
 {  
+    //TODO check for device settings here
     _device = device;
     _offset = offset;
 }
@@ -29,11 +30,8 @@ void ShellySmartmeterEmulationClass::setCurrentValues(bool dataAreValid, uint32_
     if (!dataAreValid) {
         return; // Don't waste any more time
     }
-    _currentValues.v1_7_0 = v1_7_0;
-    _currentValues.v2_7_0 = v2_7_0;
-    _currentValues.v1_8_0 = v1_8_0;
-    _currentValues.v2_8_0 = v2_8_0;
-    _currentValues.saldo = (int32_t)_currentValues.v1_7_0 - (int32_t)_currentValues.v2_7_0; //directly calculate saldo (otherwise we need some mutex around)
+
+    _currentValues.saldo = (int32_t)v1_7_0 - (int32_t)v2_7_0; //directly calculate saldo (otherwise we need some mutex around)
 }
 
 bool ShellySmartmeterEmulationClass::setEnabled(bool enabled)
@@ -47,79 +45,81 @@ bool ShellySmartmeterEmulationClass::setEnabled(bool enabled)
 }
 
 bool ShellySmartmeterEmulationClass::listenAndHandleUDP() {
-    if(udp.listen(_device.port)) {
+    if(_udp.listen(_device.port)) {
         writeEvent("INFO", "emulator", F("Shelly Smartmeter Emulator Listening on Port"), String(_device.port));
 
         //handle request
-        udp.onPacket([&](AsyncUDPPacket packet) {
-            if(_currentValues.dataAreValid) {
-                // --- JSON parsen ---
-                size_t len = packet.length();
-                char tempBuffer[len+1];
-                memcpy(tempBuffer, packet.data(), len);
-                tempBuffer[len] = '\0';
-                
-                DynamicJsonBuffer jsonBuffer(JSON_BUFFER_SIZE);
-                JsonObject& json = jsonBuffer.parseObject(tempBuffer);
-
-                if (!json.success()) {
-                    DBGOUT("[ DEBUG ] Failed to parse json\n");
-                    return;
-                }
-
-                //check for objects
-                if( !json.containsKey("id") || !json["id"].is<int>() || 
-                    !json.containsKey("method") || !json["method"].is<char*>() ||
-                    !json.containsKey("params")) 
-                {
-                    DBGOUT("[ DEBUG ] Invalid json\n");
-                    return;
-                }
-                JsonObject& params = json["params"];
-                if (!params.containsKey("id") || !params["id"].is<int>()) {
-                    DBGOUT("[ DEBUG ] Invalid json\n");
-                    return;
-                }
-                
-                int id = json["id"];
-                const char* method = json["method"];
-
-                //first check if deviceID is set (done here, 'cause at init Wifi may not be available already)
-                if(_device.id == "") {
-                    _device.id = WiFi.macAddress();
-                    _device.id.replace(":", "");
-                    _device.id.toLowerCase();
-                    if(_device.id == 0) {
-                        //fallback
-                        _device.id = "aa11bb22cc33";
-                    }
-                }
-
-                jsonBuffer.clear();
-                JsonObject& responseJson = jsonBuffer.createObject();
-                responseJson["id"] = id;
-                responseJson["src"] = _device.name + "-" + _device.id;
-                responseJson["dst"] = "unknown";
-                responseJson["result"] =  jsonBuffer.createObject();
-                //the B2500 is VEEEERY picky... needs "float" formatted value with a dot
-                String saldo = String(_currentValues.saldo + _offset)+".000"; 
-                if(strcmp(method, "EM.GetStatus") == 0) {
-                    responseJson["result"]["a_act_power"] = RawJson(saldo);
-                    responseJson["result"]["b_act_power"] = RawJson("0.000");
-                    responseJson["result"]["c_act_power"] = RawJson("0.000");
-                    responseJson["result"]["total_act_power"] = RawJson(saldo);
-                } else if(strcmp(method, "EM1.GetStatus") == 0) {
-                    responseJson["result"]["act_power"] = RawJson(saldo);
-                } else {
-                    DBGOUT("[ DEBUG ] unknown method\n");
-                    return;
-                }
-
-                AsyncUDPMessage message;
-                responseJson.printTo(message);
-                udp.sendTo(message, packet.remoteIP(), packet.remotePort()); //respond directly via "packet" doesn't work
-
+        _udp.onPacket([&](AsyncUDPPacket packet) {
+            
+            if(!_currentValues.dataAreValid) {
+                return;
             }
+
+            // --- JSON parsen ---
+            size_t len = packet.length();
+            char tempBuffer[len+1];
+            memcpy(tempBuffer, packet.data(), len);
+            tempBuffer[len] = '\0';
+            
+            DynamicJsonBuffer jsonBuffer(JSON_BUFFER_SIZE);
+            JsonObject& json = jsonBuffer.parseObject(tempBuffer);
+
+            if (!json.success()) {
+                DBGOUT("[ DEBUG ] Failed to parse json\n");
+                return;
+            }
+
+            //check for objects
+            if( !json.containsKey("id") || !json["id"].is<int>() || 
+                !json.containsKey("method") || !json["method"].is<char*>() ||
+                !json.containsKey("params")) 
+            {
+                DBGOUT("[ DEBUG ] Invalid json\n");
+                return;
+            }
+            JsonObject& params = json["params"];
+            if (!params.containsKey("id") || !params["id"].is<int>()) {
+                DBGOUT("[ DEBUG ] Invalid json\n");
+                return;
+            }
+            
+            int id = json["id"];
+            String method(json["method"]);
+
+            //first check if deviceID is set (done here, 'cause at init Wifi may not be available already)
+            if(_device.id == "") {
+                _device.id = WiFi.macAddress();
+                _device.id.replace(":", "");
+                _device.id.toLowerCase();
+                if(_device.id == 0) {
+                    //fallback
+                    _device.id = "aa11bb22cc33";
+                }
+            }
+            jsonBuffer.clear();
+            JsonObject& responseJson = jsonBuffer.createObject();
+            responseJson["id"] = id;
+            responseJson["src"] = _device.name + "-" + _device.id;
+            responseJson["dst"] = "unknown";
+            responseJson["result"] =  jsonBuffer.createObject();
+            //the B2500 is VEEEERY picky... needs "float" formatted value with a dot
+            String saldo = String(_currentValues.saldo + _offset)+".000"; 
+            if(method =="EM.GetStatus") {
+                responseJson["result"]["a_act_power"] = RawJson(saldo);
+                responseJson["result"]["b_act_power"] = RawJson("0.000");
+                responseJson["result"]["c_act_power"] = RawJson("0.000");
+                responseJson["result"]["total_act_power"] = RawJson(saldo);
+            } else if(method == "EM1.GetStatus") {
+                responseJson["result"]["act_power"] = RawJson(saldo);
+            } else {
+                DBGOUT("[ DEBUG ] unknown method\n");
+                return;
+            }
+
+            AsyncUDPMessage message;
+            responseJson.printTo(message);
+            _udp.sendTo(message, packet.remoteIP(), packet.remotePort()); //respond directly via "packet" doesn't work
+
         });
     
         return true;
@@ -143,7 +143,7 @@ void ShellySmartmeterEmulationClass::disable(void)
 {
     if(_enabled) {
         writeEvent("INFO", "emulator", F("Shelly Smartmeter Emulator disabled"), "");
-        udp.close();
+        _udp.close();
     }
     _enabled = false;
 }
