9b5b160de8f23a07c10db9a5598ecacae1678ec7
Stefan Oberhumer <stefan@obssys.com>
2026-02-10 23:31:30 +0100

    Add ESPAsyncUDP library version information to stats page.

--- src/Webserver_ws_Data.cpp
+++ src/Webserver_ws_Data.cpp
@@ -635,6 +635,7 @@ static void sendStatus(AsyncWebSocketClient *client)
     root[F("library_ArduinoJson")] = ARDUINOJSON_VERSION;
     root[F("library_AsyncMqttClient")] = "0.9.0";
     root[F("library_AsyncPing_esp8266")] = "95ac7e4ce1d4b41087acc0f7d8109cfd1f553881";
+    root[F("library_ESPAsyncUDP")] = "0.0.0-alpha+sha.697c75a025";
     root[F("library_ESPAsyncTCP")] = "2.0.0";
     root[F("library_ESPAsyncWebServer")] = ASYNCWEBSERVER_VERSION;
 
