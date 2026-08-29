e7b8587bfaa9b1620ee10df58fcbb4bd910aae72
GerhardMitterbaur <63344758+mgerhard74@users.noreply.github.com>
2026-08-29 17:17:27 +0200

    Undo last change
    Removed unnecessary check for zero entries in noOfPages function.

--- src/Logfile.cpp
+++ src/Logfile.cpp
@@ -474,10 +474,6 @@ uint32_t LogfileClass::noOfPages(uint32_t entriesPerPage)
 {
     uint32_t entries = noOfEntries();
 
-    if (entries == 0) {
-        return 1;
-    }
-
     if (entries % entriesPerPage) {
         entries += entriesPerPage - 1;
     }
